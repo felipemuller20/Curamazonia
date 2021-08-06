eb7d1edda8d968c6235ddfd7509d43cd4ad4d9dc
Felipe Muller <felipeavmuller@gmail.com>
2021-08-06 20:41:20 -0300

    criando redeirecionamento

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Desmatamento from './pages/Desmatamento';
 import Doar from './pages/Doar';
 import FaunaFlora from './pages/FaunaFlora';
 import ArvoresNativas from './pages/ArvoresNativas';
+import RedirectTo from './pages/RedirectTo';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={ About } />
+        <Route exact path='/' component={ RedirectTo } />
+        <Route exact path='/Curamazonia' component={ About } />
         <Route exact path='/desmatamento' component={ Desmatamento } />
         <Route exact path='/doar' component={ Doar } />
         <Route exact path='/fauna-e-flora' component={ FaunaFlora } />
---
+++ src/pages/RedirectTo.jsx
@@ -0,0 +1,10 @@
+import { Redirect } from "react-router-dom";
+import React from 'react';
+
+function RedirectTo() {
+  return (
+    <Redirect to='/Curamazonia' />
+  );
+}
+
+export default RedirectTo;
