155fbf8fb9395f8495a0af4d041733ae7cfd43f5
Felipe Muller <felipeavmuller@gmail.com>
2021-08-08 20:29:41 -0300

    adicionado scrollbar nos textos, porem invisiveis

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -43,6 +43,47 @@ function About() {
             nisi ut aliquid ex ea commodi consequatur?
             Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse,
             quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum,Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum.Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsumSed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsumSed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsumSed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsumSed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsumSed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+            qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum
             </p>
           </div>
         </main>
